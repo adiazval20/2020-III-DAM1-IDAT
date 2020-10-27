c1cfd0f1706240d55d9534454ba873cdc43d44ab
Andy <adiazval20@gmail.com>
2020-10-27 18:49:57 -0500

    2

--- Semana 2/CalculadoraCientifica.java
+++ Semana 2/CalculadoraCientifica.java
@@ -17,7 +17,7 @@ class CalculadoraCientifica extends Calculadora {
 
     // Ejm. 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...
     public void fibonacci(int x) {
-        System.out.println("Serie de Fibonacci");
+        System.out.println("Serie de Fibonacci!");
 
         int prevNum = 0;
         int actualNum = 1;
