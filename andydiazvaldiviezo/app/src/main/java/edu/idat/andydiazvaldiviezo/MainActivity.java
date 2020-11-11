d58327e76da104976b1ebb5e5684bb47f1930661
Andy <adiazval20@gmail.com>
2020-11-11 18:32:56 -0500

    0

---
+++ andydiazvaldiviezo/app/src/main/java/edu/idat/andydiazvaldiviezo/MainActivity.java
@@ -0,0 +1,17 @@
+package edu.idat.andydiazvaldiviezo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Producto reloj = new Producto();
+        .setText(reloj.getNombre())
+    }
+}
