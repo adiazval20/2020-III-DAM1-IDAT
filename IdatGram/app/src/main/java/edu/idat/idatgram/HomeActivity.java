de522519a52531f21559884b0048db1651ca9309
Andy <adiazval20@gmail.com>
2020-12-15 23:00:45 -0500

    2

--- IdatGram/app/src/main/java/edu/idat/idatgram/HomeActivity.java
+++ IdatGram/app/src/main/java/edu/idat/idatgram/HomeActivity.java
@@ -14,5 +14,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 }
