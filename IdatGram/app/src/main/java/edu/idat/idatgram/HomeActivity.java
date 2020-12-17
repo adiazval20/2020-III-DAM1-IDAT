365172bab5a3bb0ead2d9b68772218fba5d7b0f5
Andy <adiazval20@gmail.com>
2020-12-16 19:01:04 -0500

    1

--- IdatGram/app/src/main/java/edu/idat/idatgram/HomeActivity.java
+++ IdatGram/app/src/main/java/edu/idat/idatgram/HomeActivity.java
@@ -1,12 +1,18 @@
 package edu.idat.idatgram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -16,5 +22,37 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+
+        BottomNavigationView bnvSecciones = findViewById(R.id.bnvSecciones);
+        bnvSecciones.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                loadFragment(item.getItemId());
+                return true;
+            }
+        });
+
+        loadFragment(R.id.optInicio);
+    }
+
+    private void loadFragment(int itemId) {
+        Fragment fragment = new Fragment();
+
+        switch (itemId) {
+            case R.id.optInicio:
+                fragment = new InicioFragment();
+                break;
+            case R.id.optBuscar:
+                fragment = new BuscarFragment();
+                break;
+            case R.id.optPerfil:
+                fragment = new PerfilFragment();
+                break;
+        }
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frmSeccion, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
