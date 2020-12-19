6d227ca3f319e650df1c6dbd75d698d57a490e4a
Andy <adiazval20@gmail.com>
2020-12-19 18:16:44 -0500

    5

--- IdatGram/app/src/main/java/edu/idat/idatgram/PerfilFragment.java
+++ IdatGram/app/src/main/java/edu/idat/idatgram/PerfilFragment.java
@@ -1,58 +1,22 @@
 package edu.idat.idatgram;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PerfilFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PerfilFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public PerfilFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PerfilFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PerfilFragment newInstance(String param1, String param2) {
-        PerfilFragment fragment = new PerfilFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -60,5 +24,23 @@ public class PerfilFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_perfil, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        LinearLayout lytPerfil = view.findViewById(R.id.lytPerfil);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        ObjectAnimator translationY = ObjectAnimator.ofFloat(lytPerfil, "translationY", 400f, 0f);
+        translationY.setDuration(1500);
+//        translationY.start();
+
+        ObjectAnimator fade = ObjectAnimator.ofFloat(lytPerfil, "alpha", 0f, 1f);
+        fade.setDuration(1500);
+
+        animatorSet.play(translationY);
+        animatorSet.play(fade);
+        animatorSet.start();
     }
 }
