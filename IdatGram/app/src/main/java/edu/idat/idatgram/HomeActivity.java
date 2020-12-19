6e5026134e86687007ba57f3db631bc6c808af75
Andy <adiazval20@gmail.com>
2020-12-19 17:23:06 -0500

    3

--- IdatGram/app/src/main/java/edu/idat/idatgram/HomeActivity.java
+++ IdatGram/app/src/main/java/edu/idat/idatgram/HomeActivity.java
@@ -14,12 +14,23 @@ import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.HashMap;
+
 public class HomeActivity extends AppCompatActivity {
+    private HashMap<Integer, Integer> posicionesFragment;
+    private int ultimaPosicionSeleccionada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ultimaPosicionSeleccionada = 0;
+
+        posicionesFragment = new HashMap<>();
+        posicionesFragment.put(R.id.optInicio, 1);
+        posicionesFragment.put(R.id.optBuscar, 2);
+        posicionesFragment.put(R.id.optPerfil, 3);
 
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
 
@@ -28,6 +39,7 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 loadFragment(item.getItemId());
+                ultimaPosicionSeleccionada = posicionesFragment.get(item.getItemId());
                 return true;
             }
         });
@@ -36,24 +48,36 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void loadFragment(int itemId) {
-        Fragment fragment = new Fragment();
+        int posicionActual = posicionesFragment.get(itemId);
+        if (posicionActual != ultimaPosicionSeleccionada) {
+            Fragment fragment = new Fragment();
 
-        switch (itemId) {
-            case R.id.optInicio:
-                fragment = new InicioFragment();
-                break;
-            case R.id.optBuscar:
-                fragment = new BuscarFragment();
-                break;
-            case R.id.optPerfil:
-                fragment = new PerfilFragment();
-                break;
+            switch (itemId) {
+                case R.id.optInicio:
+                    fragment = new InicioFragment();
+                    break;
+                case R.id.optBuscar:
+                    fragment = new BuscarFragment();
+                    break;
+                case R.id.optPerfil:
+                    fragment = new PerfilFragment();
+                    break;
+            }
+
+            int animacionIn, animacionOut;
+            if (posicionActual > ultimaPosicionSeleccionada) {
+                animacionIn = R.anim.slide_right_to_left_in;
+                animacionOut = R.anim.slide_right_to_left_out;
+            } else {
+                animacionIn = R.anim.slide_left_to_right_in;
+                animacionOut = R.anim.slide_left_to_right_out;
+            }
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.setCustomAnimations(animacionIn, animacionOut);
+            transaction.replace(R.id.frmSeccion, fragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
         }
-
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.setCustomAnimations(R.anim.slide_left_to_right_in, R.anim.slide_left_to_right_out);
-        transaction.replace(R.id.frmSeccion, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
     }
 }
